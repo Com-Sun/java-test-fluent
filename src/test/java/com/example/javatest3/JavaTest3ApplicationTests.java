49b1628dd7c0bb15bf16b4f44814c3a1cf718576
gnlslfflffl <hyunjin.jo@nhnenter.com>
2024-06-14 09:24:17 +0900

    fail test2

--- src/test/java/com/example/javatest3/JavaTest3ApplicationTests.java
+++ src/test/java/com/example/javatest3/JavaTest3ApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.javatest3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JavaTest3ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class JavaTest3ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
